13cc78f0f5b7071c01ee8391e51fde2154f850dc
JoshuaOHanlon <josh.t.ohanlon@gmail.com>
2021-02-14 03:23:44 +0000

    Set up basic client/server environment - first commit.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import react from 'React';
+
+class App extends react.component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        Working
+      </div>
+    );
+  }
+}
+
+export default App;
